973e0b6d6b8ee2200a56eb25dfbefd993ff3edb4
Chandan Singh <chandan.may2@gmail.com>
2016-09-21 02:14:42 +0530

    first commit

---
+++ src/com/crunchify/controller/CrunchifyHelloWorld.java
@@ -0,0 +1,30 @@
+package com.crunchify.controller;
+ 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+ 
+/*
+ * author: Crunchify.com
+ * 
+ */
+ 
+@Controller
+public class CrunchifyHelloWorld {
+ 
+	@RequestMapping("/welcome")
+	public ModelAndView helloWorld() {
+ 
+		String message = "<br><div style='text-align:center;'>"
+				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
+		return new ModelAndView("welcome", "message", message);
+	}
+	
+	@RequestMapping("/welcome1")
+	public ModelAndView helloWorld1() {
+ 
+		String message = "<br><div style='text-align:center;'>"
+				+ "<h3>********** Hello World, Spring MVC Tutorial1</h3>This message is coming from CrunchifyHelloWorld.java1 **********</div><br><br>";
+		return new ModelAndView("welcome1", "message", message);
+	}
+}
